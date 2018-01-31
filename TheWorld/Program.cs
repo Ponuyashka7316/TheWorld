8e2203b6f1875425052c2f624c5e2898dbe0e22f
wtwet <yuriygurin1@mail.ru>
2018-01-31 18:00:14 +0200

    Start creating an index page

--- TheWorld/Program.cs
+++ TheWorld/Program.cs
@@ -19,6 +19,7 @@ namespace TheWorld
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            
                 .UseStartup<Startup>()
                 .Build();
     }
