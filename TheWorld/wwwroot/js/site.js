af59af57b59ccbaddbf0918d61f1126f96f0d51a
wtwet <yuriygurin1@mail.ru>
2018-01-31 20:48:40 +0200

    Add some css and js

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿
+(function () {
+   //var ele = $("#username");
+   // ele.text("hello");
+   // $("#main").mouseenter(function () {
+   //     this.style = "background-color:#888";
+   // }).mouseleave(function () {
+   //     this.style = "";
+   //     });
+   // $("ul.menu li a").on("click", function () {
+   //     alert($(this).text());
+   // })
+
+   // var menuItems = $("ul.menu li a");
+   // menuItems.on("click", function () {
+   //     var me = $(this);
+   //     alert(me.text());
+   // });
+
+    var $sideBarAndWrapper = $("#sidebar, #wrapper");
+
+    $('#sideBarToggle').on('click', function () {
+        $sideBarAndWrapper.toggleClass("hide-sidebar");
+        if ($sideBarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show  sidebar");
+        } else {
+            $(this).text("Hide  sidebar");
+        }
+    });
+})();
